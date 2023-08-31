801452f9d0db88e775599fb8f10db23ed8bd4715
Luiz Antoniassi <luiz.antoniassi7@gmail.com>
2023-08-31 17:09:21 -0300

    Formalização do primeiro teste do Test Plan

---
+++ Leon3 Development/Software/Veritas_Test_EMUREAL/src/main.c
@@ -0,0 +1,63 @@
+/* Inclusão de bibliotecas */
+
+#include <stdio.h>
+#include <drv/apbuart.h>
+#include <string.h>
+#include "include/uart.h"
+
+int main(void){
+
+/* 	Declaração das variáveis principais */
+
+	struct apbuart_priv *device;
+	uint32_t cont;
+	struct apbuart_config cfg;
+	char strSend[MAX_STRING], strReceive[MAX_STRING], strAdd[] = "Hello again!";
+
+/* Inicialização dos drivers da APBUART e inicialização da APBUART 0 */
+
+	apbuart_autoinit();
+
+	device = apbuart_open(0);
+
+	if (!device)
+	{
+		printf("DEVICE ERROR\n");
+		return -1;
+	}
+
+/* Configurações da APBUART */
+
+	cfg.baud = 9600;
+	cfg.parity = APBUART_PAR_NONE; // UART_PAR_NONE
+	cfg.flow = 0;
+	cfg.mode = APBUART_MODE_NONINT; //UART_MODE_NONINT
+
+	apbuart_config(device, &cfg);
+
+/* Recebimento dos dados */
+
+    apbuartReceiveString(device,strReceive,1,0x21);
+
+    strcpy(strSend,strReceive);
+
+/* Transformação da massa de dados recebida */
+
+    for(cont = 0; cont < strlen(strAdd); cont++){
+
+    	strSend[strlen(strReceive) + cont + 1] = strAdd[cont];
+
+    }
+
+/* Reenvio da massa de dados transformada */
+
+    apbuartSendString(device, strSend);
+
+/* Fechamento do APBUART aberta */
+
+	apbuart_close(device);
+
+	return 0;
+}
+
+
---
+++ Leon3 Development/Software/Veritas_Test_EMUREAL/src/uart.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <drv/apbuart.h>
+#include <string.h>
+#include "include/uart.h"
+
+void apbuartSendString(struct apbuart_priv *device, char strSend[MAX_STRING]){
+
+/* Inicializa as variáveis */
+
+	uint32_t strLen, cont, confirm, statsRegister;
+	const uint32_t mask = (1 << 1) | (1 << 2);
+
+/* Contabiliza o número de bytes da string */
+
+	strLen = strlen(strSend);
+
+/* Realiza um laço de repetição para envio de byte a byte */
+
+	for(cont =0 ;cont < strLen;cont++){
+
+          confirm = 0;
+
+          while(confirm != 1) confirm = apbuart_outbyte(device, strSend[cont]); // Enquanto o byte não for enviado, continua no laço de repetição
+
+	}
+
+/* Aguarda a transmissão da informação -> FIFO e Shift Register vazios */
+
+        do{
+
+          statsRegister = apbuart_get_status(device);
+          
+          statsRegister = statsRegister & mask;
+
+        }while(statsRegister != 6);
+
+	return;
+}
+
+
+void apbuartReceiveString(struct apbuart_priv *device, char strReceive[MAX_STRING], uint32_t cntrl, uint32_t stopnumBytes){
+
+/* Inicializa as variáveis */
+
+	uint32_t cont = 0, confirm, statsRegister;
+    const uint32_t mask = (0b111111 << 26);
+
+/* Dita a maneira de recebimento da string a partir da variável 'control' */
+
+	switch(cntrl){
+
+/* Para o caso 0, limita o recebimento da string pelo número de bytes informado */
+
+	  case 0:
+
+	     	for(cont=0;cont < stopnumBytes;cont++){
+
+          	  confirm = -1;
+
+          	  while(confirm == -1){
+          		 confirm = apbuart_inbyte(device);
+          		 strReceive[cont] = confirm;
+          	  }
+		}
+
+	     	break;
+
+/* Para o caso 1, limita o recebimento da string pelo stopbyte informado */
+
+	  case 1:
+
+	     	do{
+
+          	  confirm = -1;
+
+          	  while(confirm == -1) confirm = apbuart_inbyte(device);
+
+                  strReceive[cont] = confirm;
+
+                  cont++;
+
+	        }while(confirm != stopnumBytes);
+
+	     	break;
+
+	}
+
+/* Aguarda o recebimento da informação -> FIFO e Shift Register vazios */
+
+
+        do{
+
+          statsRegister = apbuart_get_status(device);
+          
+          statsRegister = statsRegister & mask;
+
+        }while(statsRegister != 0);
+	
+
+	return;    
+}
+
+
+
+
+
