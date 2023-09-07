d4daaf1192c74844a53d138aadb6ee22df01072e
Luiz Antoniassi <luiz.antoniassi7@gmail.com>
2023-09-06 22:07:05 -0300

    Adição da SerialSocketLibrary e modificações extras

--- Auxiliary Development/pythonConnectTCP/EMUEMU_And_EMUREAL_UART_Tests/EMUREAL_UART_Communication/EMUREAL_Test1.py
+++ Auxiliary Development/pythonConnectTCP/EMUEMU_And_EMUREAL_UART_Tests/EMUREAL_UART_Communication/EMUREAL_Test1.py
@@ -10,87 +10,17 @@ import time
 import os
 from socket import *
 from random import *
+from SerialSocketLibrary import *
 
 
 ##################################################################################################################################################################################
 ##################################################################################################################################################################################
 # Definição de funções úteis ao código
 
-def serialConnectSerial():
-
-   listStrPort = []
-   i = 0
-
-   listPort = serial.tools.list_ports.comports()
-      
-   for port in listPort:
-
-      listStrPort.append(str(port))
-
-   for n in listStrPort:
-                
-     print(f"{i} -> {n}")
-                
-     i+=1
-            
-   select_port = input("\nSelecione uma porta com base no número: ")
-
-   try:
-      
-      serialComn = serial.Serial()
-
-      serialComn.baudrate = 9600
-
-      serialComn.port = listStrPort[int(select_port)][:5]
-
-      serialComn.open()
-
-      print("Sistema conectado à porta serial '%s'. \n" % serialComn.port)
-
-      return serialComn
-
-   except:
-
-      print("Não foi possível conectar à porta desejada...")
-
-      return False
-   
-
-def sendSerial(serialComn,string):
-   # Responsável por enviar uma string de dados pela conexão serial
-   
-   serialComn.write(string.encode())
-
-   return
-
-
-def strReceiveSerial(serialComn,stopByte,numBytes):
-   # Responsável por receber uma mensagem a partir da conexão serial, retorna a string recebida.
-   # Para numBytes = 0, recebimento de string é delimitado por stopByte.
-   # Para numBytes = 1, recebimento de string é delimitado pelo próprio numBytes.
-
-   string = ""
-
-   if(numBytes == 0):
-      
-      byteAdd = serialComn.read().decode("utf-8")
-
-      while(byteAdd != stopByte):
-
-        string = string + byteAdd
-        byteAdd = serialComn.read().decode("utf-8")
-
-   else:
-
-      for i in range(1,numBytes):
-
-         string = string + serialComn.read().decode("utf-8")
-
-   return string
-
-
 def strDataGenerator(numData):
-   # Função destinada à geração a de uma string de letras e números aleatórios, segundo um determinado parâmetro size.
+   # Desc: Função destinada à geração a de uma string de letras e números aleatórios, segundo um determinado parâmetro size.
+   # Return: String a ser formada.
+   # Paramaters: numData --> Número de bytes a ser formada a string.
 
    string = ""
    characters = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -104,6 +34,10 @@ def strDataGenerator(numData):
 
 
 def strCipherCaesar(string, numOffset):
+   # Desc: Função destinada à transformação de uma string em outra por meio da Cifra de César.
+   # Return: String transformada.
+   # Paramaters: string --> String a ser transformada.
+   #             numOffset --> Offset a ser utilizado na Cifra de César.
 
    characters = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
    
@@ -117,15 +51,16 @@ def strCipherCaesar(string, numOffset):
 
    return strCipher
 
-def bTestMaker(serialComn, numData, numOffset, stopByte, numBytes):
-   # Função responsável pela realização individual de testes, aguardando e validando segundo a cifra de César escolhida.
-   # Retorna True se o teste for validado corretamente, e retorna False se o teste falhar
+def bTestMaker(type,cntrl,serialComn, numData, numOffset, stopByte, numBytes):
+   # Desc: Função destinada à elaboração e validação de um único teste.
+   # Return: Resultado do teste (True --> Teste sucedido e False --> Teste fracassado)
+   # Paramaters: type --> 
 
    strSend = strDataGenerator(numData)
 
-   sendSerial(serialComn, strSend)
+   writeSerial(serialComn, strSend)
 
-   strReceive = strReceiveSerial(serialComn,stopByte,numBytes)
+   strReceive = strReadSerial(cntrl,serialComn,stopByte,numBytes)
 
    if strReceive == strCipherCaesar(strSend, numOffset):
 
@@ -154,7 +89,7 @@ failTests = 0
 
 print("Bem vindo ao Test Code do Test 1! \n")
 
-serialComn = serialConnectSerial()
+serialComn = serialSocketConnect("serial")
 
 numTest = int(input("Digite o número de testes a serem realizados: "))
 
@@ -165,11 +100,11 @@ os.system("cls")
 print("Iniciando os testes...")
 time.sleep(1.5)
 
-for i in range(0, numTest - 1):
+for cont in range(0, numTest - 1):
 
-   print("TESTE %i" % i)
+   print("TESTE %i" % cont)
    
-   if bTestMaker(serialComn, numData, 25, '', 10) == True:
+   if bTestMaker("numBytes",serialComn, numData, 25, '', 10) == True:
 
       print("Sucesso!\n")
       okTests += 1
---
+++ Auxiliary Development/pythonConnectTCP/EMUEMU_And_EMUREAL_UART_Tests/EMUREAL_UART_Communication/SerialSocketLibrary.py
@@ -0,0 +1,184 @@
+##################################################################################################################################################################################
+##################################################################################################################################################################################
+# Import de bibliotecas necessárias
+
+
+import serial
+import threading
+import serial.tools.list_ports
+import time
+import os
+from socket import *
+from random import *
+
+
+
+##################################################################################################################################################################################
+##################################################################################################################################################################################
+# Definição de funções úteis ao código
+
+
+def serialSocketConnect(cntrl):
+   # Desc.: Função utilizada para a conexão do programa com uma porta serial ou com um servidor socket.
+   # Return: se a conexão for realizada, retorna a porta serial serialComn ou o socket. Se não, retorna False.
+   # Parameters: cntrl -> Se for igual à string "serial", programa conecta-se a uma porta serial.
+   #                      Se for igual à string "socket", programa conecta-se a um servidor socket.
+
+   listStrPort = []
+   cont = 0
+
+   # Repartição da função destinada à conexão com porta serial
+
+   if cntrl == "serial":
+
+      # Encontra a lista de portas seriais disponíveis e cria outra com os nomes destas
+
+      listPort = serial.tools.list_ports.comports()
+      
+      for port in listPort:
+
+         listStrPort.append(str(port))
+
+      for n in listStrPort:
+                
+        print(f"{cont} -> {n}")
+                
+        cont+=1
+
+      # Informa ao usuário as portas e pede para este escolher
+            
+      select_port = input("\nSelecione uma porta com base no número: ")
+
+      # Conecta à porta serial, retornando a porta se conseguir e False se não
+
+      try:
+      
+         serialComn = serial.Serial()
+
+         serialComn.baudrate = 9600
+
+         serialComn.port = listStrPort[int(select_port)][:5]
+
+         serialComn.open()
+
+         print("Sistema conectado à porta serial '%s'. \n" % serialComn.port)
+
+         return serialComn
+
+      except:
+
+         print("Não foi possível conectar à porta desejada...")
+
+         return False
+      
+   # Repartição destinada para conexão com socket
+
+   elif cntrl == "socket":
+      
+      tcpPort = int(input("Digite a porta TCP a ser conectada: "))
+
+      host = input("Digite o host a ser conectado: ")
+
+      sock = socket(AF_INET6, SOCK_STREAM)
+
+      try:
+
+         sock.connect((host, tcpPort))
+
+         print("Sistema conectado à porta TCP (%s, %i). \n" % (host, tcpPort))
+
+         return sock
+
+      except:
+
+         print("Não foi possível conectar à porta desejada...")
+
+         return False
+      
+
+def writeSerial(serialComn,string):
+   # Desc: Função utilizada para enviar uma string por uma porta serial.
+   # Return: (-)
+   # Parameters: serialComn --> Objeto da classe serial representando uma porta serial conectada.
+   #             string --> String a ser enviada.
+
+   serialComn.write(string.encode())
+
+   return
+
+
+def writeSocket(socket,string):
+   # Desc: Função utilizada para enviar uma string por uma porta TCP.
+   # Return: (-)
+   # Parameters: socket --> Objeto da classe socket representando uma porta TCP conectada.
+   #             string --> String a ser enviada
+
+   socket.send(string.encode())
+
+   return
+
+
+def strReadSerial(cntrl, serialComn, stopByte, numBytes):
+   # Desc: Função utilizada para receber uma string por meio de uma porta serial.
+   # Return: String a ser recebida.
+   # Parameters: cntrol --> Se for igual à string "stopByte", programa limita o recebimento da string por meio de stopByte.
+   #                        Se for igual à string "numBytes", programa limita o recebimento da string por meio de numBytes.
+   #             serialComn --> Objeto da classe serial representando uma porta serial conectada.
+   #             stopByte --> Char determinante para a limitação de recebimento da string.
+   #             numBytes --> Valor inteiro determinante para a limitação de recebimento da string.
+
+   string = ""
+
+   # Se cntrl for igual a "stopByte", então recebimento de string é delimitado pelo parâmetro stopByte
+
+   if cntrl == "stopByte":
+      
+      byteAdd = serialComn.read().decode("utf-8")
+
+      while(byteAdd != stopByte):
+
+        string = string + byteAdd
+        byteAdd = serialComn.read().decode("utf-8")
+
+   # Se cntrl for igual a "numBytes", então recebimento de string é delimitado pelo parâmetro numBytes
+
+   elif cntrl == "numBytes":
+
+      for i in range(1,numBytes):
+
+         string = string + serialComn.read().decode("utf-8")
+
+   return string
+
+
+def strReadSocket(cntrl, socket, stopByte, numBytes):
+   # Desc: Função utilizada para receber uma string por meio de uma porta TCP.
+   # Return: String a ser recebida.
+   # Parameters: cntrol --> Se for igual à string "stopByte", programa limita o recebimento da string por meio de stopByte.
+   #                        Se for igual à string "numBytes", programa limita o recebimento da string por meio de numBytes.
+   #             serialComn --> Objeto da classe serial representando uma porta serial conectada.
+   #             stopByte --> Char determinante para a limitação de recebimento da string.
+   #             numBytes --> Valor inteiro determinante para a limitação de recebimento da string.
+
+   string = ""
+
+   if cntrl == "stopByte":
+      
+      byteAdd = socket.recv(1024).decode("utf-8")
+
+      while(byteAdd != stopByte):
+
+        string = string + byteAdd
+        byteAdd = socket.recv(1024).decode("utf-8")
+
+   elif cntrl == "numBytes":
+
+      for i in range(1,numBytes):
+
+         string = string + socket.recv(1024).decode("utf-8")
+
+   return string
+      
+
+
+      
