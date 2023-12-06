bfc162a24f1ad747efa86487de47ce0c7981ea07
Luiz Antoniassi <luiz.antoniassi7@gmail.com>
2023-12-06 09:07:16 -0300

    Additions in testAhbram

--- Leon3 Development/Software/testMCTRL/src/main.c
+++ Leon3 Development/Software/testMCTRL/src/main.c
@@ -11,15 +11,26 @@ int main(void){
 /* Definição de algum ponteiro para a região da memória da ahbram definida */
 uint32_t *u32pont1 = 0xA0000000;
 uint32_t *u32pont2 = 0xA0000004;
+uint32_t *u32pont3 = 0xA0000008;
+uint32_t *u32pont4 = 0xA000000C;
+uint32_t *u32pont5 = 0xA0000010;
+uint32_t *u32pont6 = 0xA0000014;
+uint32_t *u32pont7 = 0xA0000018;
+uint32_t *u32pont8 = 0xA000001C;
+uint32_t *u32pont9 = 0xA0000020;
+
 
 
 /* Input de algum valor arbitrário naquele endereço de memória */
 *(u32pont1) = 0xF;
 *(u32pont2) = 0xA;
-
-*(u32pont1) = *(u32pont2);
-
-/* Debuggando, pode-se verificar se o valor foi capaz de ser armazenado ou não */
+*(u32pont3) = 0xC;
+*(u32pont4) = 0xC;
+*(u32pont5) = 0xF;
+*(u32pont6) = 0xA;
+*(u32pont7) = 0xC;
+*(u32pont8) = 0xC;
+*(u32pont9) = 0xF;
 
 return 1;
 
